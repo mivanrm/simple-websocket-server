ca9db341d09136965817452adab5fd5de4b0fee1
mivanrm <mivanrm12@gmail.com>
2019-11-09 21:43:23 +0700

    a

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socketserver
 import base64
-
+import sys
 import hashlib
-
+import errno
+import struct
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig()
 FIN    = 0x80
 OPCODE = 0x0f
 MASKED = 0x80
@@ -18,34 +22,45 @@ OPCODE_PING         = 0x9
 OPCODE_PONG         = 0xA
 
 class TCPhandler(socketserver.StreamRequestHandler):
+    def setup(self):
+        socketserver.StreamRequestHandler.setup(self)
+        self.keep_alive = True
+        self.handshake_done = False
+        self.valid_client = False
+    
     def handle(self):
-        self.data=self.rfile.read().strip().decode()
-        headers= self.data.split("\r\n")
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        while self.keep_alive:
+            if not self.handshake_done:
+                self.data=self.rfile.read().strip().decode()
+                headers= self.data.split("\r\n")
+                print("{} wrote:".format(self.client_address[0]))
+                print(self.data)
+                if "Connection: Upgrade" in self.data and "Upgrade: websocket" in self.data:
+                    for item in headers:
+                        if "Sec-WebSocket-Key" in item:
+                            key= item.split(" ")[1]
+                    GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+                    hash = hashlib.sha1(key.encode() + GUID.encode())
+                    response_key = base64.standard_b64encode(hash.digest()).strip()
+                    response_key=response_key.decode('ASCII')
+                    resp="HTTP/1.1 101 Switching Protocols\r\n" + \
+                    "Upgrade: websocket\r\n" + \
+                    "Connection: Upgrade\r\n" + \
+                    "Sec-WebSocket-Accept: %s\r\n\r\n"%(response_key)
+                    resp=resp.encode()
+                    self.handshake_done = self.request.send(resp.encode())
+                    self.valid_client = True
+                    self.server._new_client_(self)
+                else:
+                    self.wfile.write("HTTP/1.1 400 Bad Request\r\n" + \
+                                        "Content-Type: text/plain\r\n" + \
+                                        "Connection: close\r\n" + \
+                                        "\r\n" + \
+                                        "Incorrect request")
+            elif self.valid_client:
+                self.read_next_message()
 
-        if "Connection: Upgrade" in self.data and "Upgrade: websocket" in self.data:
-            for item in headers:
-                if "Sec-WebSocket-Key" in item:
-                    key= item.split(" ")[1]
-            GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-            hash = hashlib.sha1(key.encode() + GUID.encode())
-            response_key = base64.standard_b64encode(hash.digest()).strip()
-            response_key=response_key.decode('ASCII')
-            resp="HTTP/1.1 101 Switching Protocols\r\n" + \
-             "Upgrade: websocket\r\n" + \
-             "Connection: Upgrade\r\n" + \
-             "Sec-WebSocket-Accept: %s\r\n\r\n"%(response_key)
-            resp=resp.encode()
-            self.wfile.write(resp)
-        else:
-            self.wfile.write("HTTP/1.1 400 Bad Request\r\n" + \
-                                 "Content-Type: text/plain\r\n" + \
-                                 "Connection: close\r\n" + \
-                                 "\r\n" + \
-                                 "Incorrect request")
-
-    def read_bytes(self, num):
+def read_bytes(self, num):
         # python3 gives ordinal of byte directly
         bytes = self.rfile.read(num)
         if sys.version_info[0] < 3:
@@ -53,59 +68,59 @@ class TCPhandler(socketserver.StreamRequestHandler):
         else:
             return bytes
 
-    def read_next_message(self):
-        try:
-            b1, b2 = self.read_bytes(2)
-        except SocketError as e:  # to be replaced with ConnectionResetError for py3
-            if e.errno == errno.ECONNRESET:
-                logger.info("Client closed connection.")
-                self.keep_alive = 0
-                return
-            b1, b2 = 0, 0
-        except ValueError as e:
-            b1, b2 = 0, 0
-
-        fin    = b1 & FIN
-        opcode = b1 & OPCODE
-        masked = b2 & MASKED
-        payload_length = b2 & PAYLOAD_LEN
-
-        if opcode == OPCODE_CLOSE_CONN:
-            logger.info("Client asked to close connection.")
+def read_next_message(self):
+    try:
+        b1, b2 = self.read_bytes(2)
+    except socketserver.SocketError as e:  # to be replaced with ConnectionResetError for py3
+        if e.errno == errno.ECONNRESET:
+            logger.info("Client closed connection.")
             self.keep_alive = 0
             return
-        if not masked:
-            logger.warn("Client must always be masked.")
-            self.keep_alive = 0
-            return
-        if opcode == OPCODE_CONTINUATION:
-            logger.warn("Continuation frames are not supported.")
-            return
-        elif opcode == OPCODE_BINARY:
-            logger.warn("Binary frames are not supported.")
-            return
-        elif opcode == OPCODE_TEXT:
-            opcode_handler = self.server._message_received_
-        elif opcode == OPCODE_PING:
-            opcode_handler = self.server._ping_received_
-        elif opcode == OPCODE_PONG:
-            opcode_handler = self.server._pong_received_
-        else:
-            logger.warn("Unknown opcode %#x." % opcode)
-            self.keep_alive = 0
-            return
+        b1, b2 = 0, 0
+    except ValueError as e:
+        b1, b2 = 0, 0
 
-        if payload_length == 126:
-            payload_length = struct.unpack(">H", self.rfile.read(2))[0]
-        elif payload_length == 127:
-            payload_length = struct.unpack(">Q", self.rfile.read(8))[0]
+    fin    = b1 & FIN
+    opcode = b1 & OPCODE
+    masked = b2 & MASKED
+    payload_length = b2 & PAYLOAD_LEN
 
-        masks = self.read_bytes(4)
-        message_bytes = bytearray()
-        for message_byte in self.read_bytes(payload_length):
-            message_byte ^= masks[len(message_bytes) % 4]
-            message_bytes.append(message_byte)
-        opcode_handler(self, message_bytes.decode('utf8'))
+    if opcode == OPCODE_CLOSE_CONN:
+        logger.info("Client asked to close connection.")
+        self.keep_alive = 0
+        return
+    if not masked:
+        logger.warn("Client must always be masked.")
+        self.keep_alive = 0
+        return
+    if opcode == OPCODE_CONTINUATION:
+        logger.warn("Continuation frames are not supported.")
+        return
+    elif opcode == OPCODE_BINARY:
+        logger.warn("Binary frames are not supported.")
+        return
+    elif opcode == OPCODE_TEXT:
+        opcode_handler = self.server._message_received_
+    elif opcode == OPCODE_PING:
+        opcode_handler = self.server._ping_received_
+    elif opcode == OPCODE_PONG:
+        opcode_handler = self.server._pong_received_
+    else:
+        logger.warn("Unknown opcode %#x." % opcode)
+        self.keep_alive = 0
+        return
+
+    if payload_length == 126:
+        payload_length = struct.unpack(">H", self.rfile.read(2))[0]
+    elif payload_length == 127:
+        payload_length = struct.unpack(">Q", self.rfile.read(8))[0]
+
+    masks = self.read_bytes(4)
+    message_bytes = bytearray()
+    for message_byte in self.read_bytes(payload_length):
+        message_byte ^= masks[len(message_bytes) % 4]
+        message_bytes.append(message_byte)
+    opcode_handler(self, message_bytes.decode('utf8'))
 
     def send_message(self, message):
         self.send_text(message)
@@ -118,20 +133,6 @@ class TCPhandler(socketserver.StreamRequestHandler):
         Important: Fragmented(=continuation) messages are not supported since
         their usage cases are limited - when we don't know the payload length.
         """
-
-        # Validate message
-        if isinstance(message, bytes):
-            message = try_decode_UTF8(message)  # this is slower but ensures we have UTF-8
-            if not message:
-                logger.warning("Can\'t send message, message is not valid UTF-8")
-                return False
-        elif sys.version_info < (3,0) and (isinstance(message, str) or isinstance(message, unicode)):
-            pass
-        elif isinstance(message, str):
-            pass
-        else:
-            logger.warning('Can\'t send message, message has to be a string or bytes. Given type is %s' % type(message))
-            return False
 
         header  = bytearray()
         payload = encode_to_UTF8(message)
@@ -159,6 +160,16 @@ class TCPhandler(socketserver.StreamRequestHandler):
             return
 
         self.request.send(header + payload)
+    def encode_to_UTF8(data):
+        try:
+            return data.encode('UTF-8')
+        except UnicodeEncodeError as e:
+            logger.error("Could not encode data to UTF-8 -- %s" % e)
+            return False
+        except Exception as e:
+            raise(e)
+            return False
+
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
 
